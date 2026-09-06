0044d39c9a87b665ef4537b0ce1f369daa10f16b
Pritom Biswas <pritam020s2@gmail.com>
2026-09-06 18:19:12 +0600

    chore: update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['@libsql/client']
-  }
+    serverComponentsExternalPackages: ['@libsql/client'],
+    optimizePackageImports: ['@phosphor-icons/react', '@phosphor-icons/react/dist/ssr'],
+  },
 };
 export default nextConfig;
